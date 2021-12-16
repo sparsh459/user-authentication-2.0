c2c49b0927afe6b00a0eedee5718ee5bdac4f51e
Sparsh Saxena <86906056+sparsh459@users.noreply.github.com>
2021-12-16 18:47:36 +0530

    Update info.py

--- user_auth/info.py
+++ user_auth/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'vibranttmedia@gmail.com'
-EMAIL_HOST_PASSWORD = 'T@sp23456'
+EMAIL_HOST_USER = 'abcd1234@gmail.com'
+EMAIL_HOST_PASSWORD = 'Password'
 EMAIL_PORT = 587
